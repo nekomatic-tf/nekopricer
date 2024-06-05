99dd0646e57c720f0a3acb0ad85a3f052a18fe4e
Junior <42356930+juniorISO69960@users.noreply.github.com>
2024-06-04 22:52:43 -0500

    ✅ finished MinIOEngine

---
+++ src/storage.py
@@ -0,0 +1,43 @@
+# MinIO engine
+
+import logging
+import io
+from minio import Minio
+
+class MinIOEngine:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, config: dict):
+        self.bucket = config["bucket"]
+        self.client = Minio(
+            endpoint=config["endpoint"],
+            access_key=config["key"],
+            secret_key=config["secret"],
+            secure=config["secure"]
+        )
+
+        if not self.client.bucket_exists(self.bucket):
+            self.client.make_bucket(self.bucket)
+            self.logger.debug("Created bucket " + self.bucket)
+    
+    def write_file(self, file: str, content: str):
+        data = io.BytesIO(content.encode(encoding="utf-8"))
+        self.client.put_object(
+            bucket_name=self.bucket,
+            object_name=file,
+            data=data,
+            length=len(content)
+        )
+        self.logger.info("Wrote " + file)
+
+    def read_file(self, file: str):
+        try:
+            content = self.client.get_object(
+                bucket_name=self.bucket,
+                object_name=file
+            )
+            self.logger.info("Read file " + file)
+            return content.data.decode(encoding="utf-8")
+        except Exception as e:
+            self.logger.error("Error reading file " + file + ": " + str(e))
+            return e
