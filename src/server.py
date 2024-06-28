c938555e2d860215480d5ff32f96358f29e195c1
Junior <42356930+juniorISO69960@users.noreply.github.com>
2024-06-27 21:02:57 -0500

    better check_ and get_ item function

--- src/server.py
+++ src/server.py
@@ -25,38 +25,28 @@ def get_items():
     logger.info(f"Got pricelist.")
     return pricelist.pricelist
 @app.get("/items/<sku>")
+@app.post("/items/<sku>")
 def get_item(sku: str):
     price = pricelist.get_price(sku)
-    if not price == None:
+    if not price == None: # Item exists, but price it just to keep it up to date
+        pricer.price_item({
+            "sku": price["sku"],
+            "name": price["name"]
+        })
+        price = pricelist.get_price(sku)
         return price
-    # Item doesn't exist, add and price
-    try:
+    try: # Item doesn't exist, attempt to price, then add or fail
         pricer.price_item({
             "sku": sku,
             "name": pricelist.to_name(sku)
         })
         price = pricelist.get_price(sku)
-        if not price == None: # Item is now priced, and added to the item_list, ready to roll
+        if not price == None: # Item validated as priced, go ahead and add it to the item list
             pricelist.add_item(price["name"])
             return price 
-        return Response(status=404) # Failed to price etc etc
+        return Response(status=404) # Functionally failed to price
     except Exception as e:
-        logger.error(f"Failed to add {sku}: {e}")
-        return Response(status=500)
-@app.post("/items/<sku>")
-def check_item(sku: str):
-    try:
-        pricer.price_item({
-            "sku": sku,
-            "name": pricelist.to_name(sku)
-        })
-        price = pricelist.get_price(sku)
-        if not price == None:
-            logger.info(f"Checked price for {price["name"]}/{price["sku"]}.")
-            return price
-        return Response(status=404) # Return a 404 if we failed to functionally price the item
-    except Exception as e:
-        logger.info(f"Failed to check price for {sku}: {e}")
+        logger.error(f"Failed to add {sku}: {e}") # Server error (duh)
         return Response(status=500)
 
 def init(
