8d90d746a8bc9e93b3f6c3b4ebdab30664b088a7
Junior <42356930+juniorISO69960@users.noreply.github.com>
2024-09-25 12:56:44 -0500

    Create currencies.py

---
+++ src/currencies.py
@@ -0,0 +1,129 @@
+import math
+class Currencies:
+    # Creates a new Currencies instance
+    def __init__(self, currencies: dict):
+        if currencies == None:
+            raise Exception("Missing currencies object")
+        
+        self.keys = float(currencies.get("keys") or 0)
+        self.metal = float(currencies.get("metal") or 0)
+
+        if math.isnan(self.keys) or math.isnan(self.metal):
+            raise Exception("Not a valid currencies object")
+        
+        self.metal = self.toRefined(self.toScrap(self.metal))
+    # Get the value of the currencies in scrap
+    def toValue(self, conversion: float) -> float:
+        if conversion == None and not self.keys == 0:
+            # The conversion rate is needed because there are keys
+            raise Exception("Missing conversion rate for keys in refined")
+        
+        value = self.toScrap(self.metal)
+        if not self.keys == 0:
+            value += self.keys * self.toScrap(conversion)
+        
+        return value
+    # Creates a string that represents this currencies object
+    def toString(self) -> str:
+        string = ""
+
+        if not self.keys == 0 or self.keys == self.metal:
+            # If there are keys, then we will add that to the string
+            string = f"{self.keys} key{"s" if self.keys > 1 else ""}"
+        
+        if not self.metal == 0 or self.keys == self.metal:
+            if not string == "":
+                # The string is not empty, that means that we have added the keys
+                string += ", "
+            
+            # Add the metal to the string
+            string += f"{math.trunc(self.metal * 100) / 100} ref"
+            
+        return string
+    # Creates an object that represents this currencies object
+    def toJSON(self) -> dict:
+        json = {
+            "keys": self.keys,
+            "metal": self.metal
+        }
+
+        return json
+    # Adds refined together
+    def addRefined(self, *args: float) -> float:
+        value = 0
+
+        for refined in args:
+            value += self.toScrap(refined)
+        
+        metal = self.toRefined(value)
+        return metal
+    # Rounds a number to the closest step
+    def round(self, number: float) -> float:
+        step = 0.5
+        inv = 1.0 / step
+
+        rounded = round(number * inv) / inv
+        return rounded
+    # Rounds a number up or down if the value is less than 0 or not
+    def rounding(self, number: float) -> float:
+        isPositive = number >= 0
+
+        # If we add 0.001 and it is greater than the number rounded up, then we need to round up to fix floating point error
+        rounding = round if (number + 0.001 > math.ceil(number)) else math.floor
+        rounded = rounding(abs(number))
+    
+        return rounded if isPositive else -rounded
+    # Converts scrap into a currencies object
+    def toCurrencies(self, value: float, conversion: float) -> dict:
+        if conversion == None:
+            # If the conversion rate is missing, convert the value into refined
+            metal = self.toRefined(value)
+
+            currencies = {
+                "keys": 0,
+                "metal": metal
+            }
+            return currencies
+        
+        # Convert conversion rate into scrap
+        conversion = self.toScrap(conversion)
+        # Get the highest amount of keys from the given value
+        keys = self.rounding(value / conversion)
+        # Find the value that is remaining
+        left = value - keys * conversion
+        # Convert the missing value to refined
+        metal = self.toRefined(left)
+
+        # Create a new instance of Currencies
+        currencies = {
+            "keys": keys,
+            "metal": metal
+        }
+        return currencies
+    # Coverts scrap to refined
+    def toRefined(self, scrap: float) -> float:
+        # The converstion rate between scrap and refined is 9 scrap/ref
+        refined = scrap / 9
+        # Truncate it to remove repeating decimals  (10 scrap / 9 scrap/refined = 1.1111...)
+        refined = math.trunc(refined * 100) / 100
+        return refined
+    # Coverts refined to scrap
+    def toScrap(self, refined: float) -> float:
+        # Get the estimated amount of scrap
+        scrap = refined * 9
+        # Round it to the nearest half
+        scrap = round(scrap * 2) / 2
+        return scrap
+    # Truncate a number
+    def truncate(self, number: float) -> float:
+        decimals = 2
+        # Get the factor to truncate by
+        factor = math.pow(10, decimals)
+        # Always round the number by aiming at 0
+        truncated = self.rounding(number * factor) / factor
+        return truncated
+    # Rounds the metal value to the nearest scrap
+    def getRight(self, v: float) -> float:
+        i = math.floor(v)
+        f = round((v - i) / 0.11)
+        return round(i + (f == 9 or f * 0.11), 2)
